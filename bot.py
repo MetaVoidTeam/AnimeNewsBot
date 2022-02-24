b962351faedbc3f44636337eb705f4dc0f52d872
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-02-24 23:27:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,40 +41,43 @@ bot = Client(
 @bot.on_message(filters.command("animenews")
 async def animenews(_, message): 
 
-    news_page = urlopen("https://myanimelist.net/news")
-    soup = BeautifulSoup(news_page, "html.parser")
+    new_page = urlopen("https://myanimelist.net/news")
+    soup = BeautifulSoup(new_page, "html.parser")
 
-    news = soup.findAll(
+    AnimeNews = soup.findAll(
         "div", {"class": "news-unit clearfix rect"}
     )
 
-    titles, links, description, images = [], [], [], []
+    title, image, description, newlink = [], [], [], []
 
-    for x in news:
+    for x in AnimeNews:
 
-        title = x.findChildren("p")[0]
-        titlex = title.find("a", href=True)
-        titles = titlex.get_text()
+        text = x.findChildren("p")[0]
+        a = text.find("a", href=True)
+        text = a.get_text()
 
-        newslink = titlex["href"]
+        newslinkx = a["href"]
 
         images = x.find("img")
         imagex = images["src"]
 
-        description = x.findChildren("div", {"class": "text"})[0]
+        descriptionx = x.findChildren("div", {"class": "text"})[0]
 
-        title.append(titles)
-        description.append(" ".join(desc.text.split()))
-        image.append(imagex)
+        title.append(text)
+        image.append(imagex)      
+        description.append(" ".join(descriptionx.text.split()))
+        newlink.append(newslinkx)
 
     description = description
-    titles = title
+    title = title
     buttons = [[
-        InlineKeyboardButton("Read More", url=f"{newslink[0]}")
+        InlineKeyboardButton("Read More", url=f"{newlink[0]}")
     ]]
 
     photo = f"{image[0]}"
     
-    await message.reply_photo(photo=photo, caption=f"**Title:** {titles[0]}\n**Description:** {description[0]}", reply_markup=InlineKeyboardMarkup(buttons))
+    await message.reply_photo(photo=photo, caption=f"**Title:** {title[0]}\n**Description:** {description[0]}", reply_markup=InlineKeyboardMarkup(buttons))
 
 
+
+bot.run()
